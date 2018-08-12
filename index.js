b8d806abd8e414f1bb288beb46bb985a62f1ec6e
Luffy1087 <francesco.1987@alice.it>
2018-08-12 22:50:29 +0200

    refactoring seems to work

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// var configuration = require('./configure').configuration,
-//     extend = require('extend'),
-//     prepareEnvs = require('./prepareEnv').prepareEnv,
-//     referencesFixer = require('./referencesFixer'),
-//     events = configuration.events;
-
-// function startProgram(cfg) {
-//     var args = require('./argumentsGetter').argumentsGetter(cfg);
-//     var wholeObject = extend({ events: events }, cfg, args);
-    
-//     prepareEnvs.prepareFirstEnvironment(wholeObject);
-// }
-
-// function startFixer(wholeObject) {
-    
-// }
-
-// events.once('onStart', startProgram);
-// events.once('onFirstEnvironmentFinished', function(wholeObject) { prepareEnvs.prepareSecondEnvironment(wholeObject); });
-// events.once('onSecondEnvironmentFinished', function(wholeObject) { startFixer(wholeObject); });
-// configuration.getConfig();
-
-
-//Model
-    //Configure base
-    //ArgumentGetter
-//prepareEnv => Model
-//referenceFixer => Model
-//packagesReader => Model
-
 //Dependencies
 var Base = require('./base');
 var PrepareEnv = require('./prepareEnv');
