30a4667318b439833860756bf5fe74ceecc369ce
Francesco Ferdico <francesco.ferdico@ynap.com>
2018-08-30 11:12:05 +0200

    needs to be set in allReferences

--- referencesFixer.js
+++ referencesFixer.js
@@ -1,37 +1,47 @@
 var fs = require('fs')
   , xml2js = require('xml2js');
 
+function onReceivingReferences(env, referencesArray) {
+    //referencesArray.dll
+    //referencesArray.references
+}
 
-function getJson(xml) {
+function getJson(file, xml, resolve, reject) {
     xml2js.parseString(xml, {}, function(err, json) {
-        if (!err) { return void resolveTask(null, json.ItemGroup[0].Reference); }
+        if (!err) { return void resolve(null, { dll: file.replace('.csproj', ''), references: json.ItemGroup[0].Reference } ); }
         
+        reject();
         throw new Error('error raised parsing xml');
     });
 }
 
-function getXml(file) {
+function getXml(file, resolve, reject) {
     fs.readFile(file, function(err, result) {
-        if (!err) { return void getJson.call(this, result.toString()); }
+        if (!err) { return void getJson.call(this, file, result.toString(), resolve, reject); }
         
+        reject();
         throw new Error('Xml File not found');
     });
 }
 
-function onGettingFile(files) {
+function onGettingFile(env, files) {
     var splittedDlls = this.arguments.dlls.split(',');
+    var promises = [];
     files.filter(function(file) { return this.utils.stringMatchInArray(splittedDlls, file); }, this);
+    
     for (var i = 0; file = files[i]; i++) {
-        getXml.call(this, file);
+        promises.push(new Promise(function(resolve, reject) { getXml.call(this, file, resolve, reject); }));
     }
+    
+    Promise.all(promises).then(onReceivingReferences.bind(this, env));
 }
 
-function fixReferences(path) {
-    this.utils.getProjectFiles(path, onGettingFile.bind(this));
+function start(env, path) {
+    this.utils.getProjectFiles(path, onGettingFile.bind(this, env));
 }
 
 function ReferencesFixer() {
-    this.fixReferences = fixReferences;
+    this.start = start;
     this.allReferences = {};
 }
 
